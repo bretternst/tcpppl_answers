7a075491393aa7b4b62ed042ef37c57c57c68b82
brett <brett@jacksonmaynard.com>
2012-08-29 19:54:27 -0700

    22.06

--- ch22/ex06.cpp
+++ ch22/ex06.cpp
@@ -1,121 +1,64 @@
 #include <valarray>
+#include <iostream>
 
 namespace ch22 {
     using namespace std;
 
-    template<class T>
-    class Slice_iter : public iterator<random_access_iterator_tag, T> {
-        valarray<T>* v;
-        slice s;
-        size_t curr;
-
-        T& ref(size_t i) const { return (*v)[s.start()+i*s.stride()]; }
-    public:
-        Slice_iter(valarray<T>* vv, slice ss) : v(vv), s(ss), curr(0) { }
-
-        Slice_iter end() const {
-            Slice_iter t = *this;
-            t.curr = s.size();
-            return t;
-        }
-
-        Slice_iter& operator++() { curr++; return *this; }
-        Slice_iter operator++(int) { Slice_iter t = *this; curr++; return t; }
-
-        T& operator[](size_t i) { return ref(i); }
-        T& operator()(size_t i) { return ref(i); }
-        T& operator*() { return ref(curr); }
-
-        size_t operator-(const Slice_iter& q) {
-            return curr - q.curr;
-        }
-
-        friend bool operator==(const Slice_iter& p, const Slice_iter& q) {
-            return p.curr == q.curr && p.s.stride()==q.s.stride() && p.s.start()==q.s.start();
-        }
-        friend bool operator!=(const Slice_iter& p, const Slice_iter& q) {
-            return !(p==q);
-        }
-        friend bool operator<(const Slice_iter& p, const Slice_iter& q) {
-            return p.curr < q.curr && p.s.stride()==q.s.stride() && p.s.start()==q.s.start();
-        }
-    };
-
-    template<class T>
-    class Cslice_iter : public iterator<random_access_iterator_tag, T> {
-        const valarray<T>* v;
-        slice s;
-        size_t curr;
-
-        const T& ref(size_t i) const { return (*v)[s.start()+i*s.stride()]; }
-    public:
-        Cslice_iter(const valarray<T>* vv, slice ss) : v(vv), s(ss), curr(0) { }
-        template<class U>
-        Cslice_iter(const Slice_iter<U>& x) : v(x.v), s(x.s), curr(x.curr) { }
-
-        Cslice_iter end() const {
-            Cslice_iter t = *this;
-            t.curr = s.size();
-            return t;
-        }
-
-        Cslice_iter& operator++() { curr++; return *this; }
-        Cslice_iter operator++(int) { Cslice_iter t = *this; curr++; return t; }
-
-        const T& operator[](size_t i) { return ref(i); }
-        const T& operator()(size_t i) { return ref(i); }
-        const T& operator*() { return ref(curr); }
-
-        size_t operator-(const Cslice_iter& q) {
-            return curr - q.curr;
-        }
-
-        friend bool operator==(const Cslice_iter& p, const Cslice_iter& q) {
-            return p.curr == q.curr && p.s.stride()==q.s.stride() && p.s.start()==q.s.start();
-        }
-        friend bool operator!=(const Cslice_iter& p, const Cslice_iter& q) {
-            return !(p==q);
-        }
-        friend bool operator<(const Cslice_iter& p, const Cslice_iter& q) {
-            return p.curr < q.curr && p.s.stride()==q.s.stride() && p.s.start()==q.s.start();
-        }
-    };
-
-
     class Matrix {
         valarray<double>* v;
-        size_t d1, d2;
+        size_t d1, d2, d3;
     public:
-        Matrix(size_t x, size_t y);
-        Matrix(const Matrix&);
-        Matrix& operator=(const Matrix&);
+        Matrix(size_t x, size_t y, size_t z) : d1(x), d2(y), d3(z), v(new valarray<double>(x*y*z)) { }
+        Matrix(const Matrix& x) : d1(x.d1), d2(x.d2), d3(x.d3), v(new valarray<double>(*x.v)) { }
+        Matrix& operator=(const Matrix& x);
         ~Matrix() { delete v; }
 
-        size_t size() const { return d1 * d2; }
+        size_t size() const { return d1 * d2 * d3; }
         size_t dim1() const { return d1; }
         size_t dim2() const { return d2; }
+        size_t dim3() const { return d3; }
 
-        Slice_iter<double> row(size_t i);
-        Cslice_iter<double> row(size_t i) const;
+        double& operator()(size_t x, size_t y, size_t z) { return (*v)[x + y * d1 + z * d1 * d2]; }
+        double operator()(size_t x, size_t y, size_t z) const { return (*v)[x + y * d1 + z * d1 * d2]; }
 
-        Slice_iter<double> column(size_t i);
-        Cslice_iter<double> column(size_t i) const;
-
-        double& operator()(size_t x, size_t y);
-        double operator()(size_t x, size_t y) const;
-
-        Slice_iter<double> operator()(size_t i) { return column(i); }
-        Cslice_iter<double> operator()(size_t i) const { return column(i); }
-
-        Slice_iter<double> operator[](size_t i) { return column(i); }
-        Cslice_iter<double> operator[](size_t i) const { return column(i); }
-
-        Matrix& operator*=(double);
+        Matrix& operator*=(double d) { *v *= d; return *this; }
         valarray<double>& array() { return *v; }
     };
+
+    Matrix& Matrix::operator=(const Matrix& x) {
+        delete v;
+        v = new valarray<double>(*x.v);
+        d1 = x.d1;
+        d2 = x.d2;
+        d3 = x.d3;
+    }
 }
 
 int main() {
     using namespace std;
     using namespace ch22;
+
+    Matrix m(3,3,2);
+    int count = 0;
+    for(int z = 0; z < 2; z++) {
+        for(int y = 0; y < 3; y++) {
+            for(int x = 0; x < 3; x++) {
+                m(x,y,z) = count++;
+            }
+        }
+    }
+
+    m *= 2;
+
+    for(int z = 0; z < 2; z++) {
+        cout << "Plane " << z << endl;
+        for(int y = 0; y < 3; y++) {
+            for(int x = 0; x < 3; x++) {
+                cout.width(6);
+                cout << m(x,y,z);
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
 }
