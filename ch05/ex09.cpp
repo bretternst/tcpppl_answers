3b32769e986aaade41ba6d04ac5a6f547620cf07
Gleb Ezhov <gezhov1@gmail.com>
2023-07-03 15:40:37 +0400

    ch5ex09 updated

--- ch05/ex09.cpp
+++ ch05/ex09.cpp
@@ -5,10 +5,13 @@ using namespace std;
 
 int main()
 {
-    // This question is a bit unclear.
-    // You might refer to the name of a type, for example, when
-    // using a pointer to the same type inside itself (e.g. a linked list).
+    // You might refer to the name of a type, for example, when using
+    // a pointer to the same type inside its definition (e.g. a linked list).
     struct node {
         node *next;
     };
+
+    // Name can be used in its own initializer
+    // to create cycled graph data structures like this:
+    node head = {&head};
 }
